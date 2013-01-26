eb879a88bfb41ae3bc0b5318a7ff008000d2ba5a
mildmojo <theaterfire@yahoo.com>
2013-01-26 17:14:03 -0500

    Adds (disabled) wave-blocking wall support.

--- Assets/Popup.cs
+++ Assets/Popup.cs
@@ -29,16 +29,28 @@ public class Popup : MonoBehaviour {
 	}
 */
 	void OnTriggerEnter(Collider other) {		
+		Vector3 test_ray;
+		LayerMask layerMask;
 		
-		if (!(other.gameObject.tag == "Pillar")) {			
-			float distance = Vector3.Distance(this.transform.position, other.transform.position);
-			if (this.rigidbody.velocity.z < 0) 
-				this.rigidbody.velocity = Vector3.zero;
-			this.rigidbody.AddForce(Vector3.up.normalized * popupForce);
-			//this.gameObject.rigidbody.AddForce(Vector3.up.normalized * (popupForce * (falloffDistance - distance)/falloffDistance ));
-		} else {
+		// Don't collide with other pillars.
+		if (other.gameObject.tag == "Pillar") {			
 			Debug.Log("Hit pillar?");
+			return;
 		}
+		
+		// Don't propagate through walls.
+//		if (wallBetween(other.transform.position, this.rigidbody.position)) {
+//			return;
+//		}
+		
+		// Pillar movement looks better if wave force only acts on resting or rising pillars.
+		if (this.rigidbody.velocity.z < 0) {
+			this.rigidbody.velocity = Vector3.zero;
+		}
+
+		//float distance = Vector3.Distance(this.transform.position, other.transform.position);
+		//this.gameObject.rigidbody.AddForce(Vector3.up.normalized * (popupForce * (falloffDistance - distance)/falloffDistance ));
+		this.rigidbody.AddForce(Vector3.up.normalized * popupForce);
 	}
 
 	
@@ -48,4 +60,14 @@ public class Popup : MonoBehaviour {
 
 	}
 	
+	void xzPlane(Vector3 vector) {
+		return Vector3(vector.x, 0, vector.y);
+	}
+
+	bool wallBetween(Vector3 vect1, Vector3 vect2) {
+		Vector3 test_ray = xzPlane(vect1) - xzPlane(vect2);
+		LayerMask layerMask = LayerMask(LayerMask.NameToLayer('Walls'));
+		return Physics.Raycast(test_ray, test_ray.sqrMagnitude, layerMask);
+	}
+
 }
