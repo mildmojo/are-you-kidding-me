8d80833c996cfcbdd66f235fa2dce4e5039fb30c
mildmojo <mildmojo@users.noreply.github.com>
2013-01-26 22:40:21 -0500

    Scales spike rotation to FPS, reduces ScaleEvent size.

--- Assets/LevelBuilder.cs
+++ Assets/LevelBuilder.cs
@@ -13,6 +13,9 @@ public class LevelBuilder : MonoBehaviour {
 	public GUIText text;
 	public ParticleSystem particles;
 	public Light exitLight;
+
+	[HideInInspector]
+	public Vector3 levelDimensions;
 	
 	private IList allLevels;
 	
@@ -171,6 +174,8 @@ public class LevelBuilder : MonoBehaviour {
 	//	}
 			
 		level = (int [,]) allLevels[currentLevel];
+		levelDimensions = new Vector3(level.GetLength(0), 0, level.GetLength(1));
+		
 		for (int i = 0; i < 20;i++) {
 			for (int j = 0; j < 20; j++) {		
 				if (level[i,j] == 1) {					
--- Assets/Mesh/Materials/Rotate.cs
+++ Assets/Mesh/Materials/Rotate.cs
@@ -10,7 +10,7 @@ public class Rotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.Rotate(new Vector3(0,0,1), 0.2f);
+		this.transform.Rotate(new Vector3(0,0,1), Time.deltaTime * 15f);
 	
 	}
 }
