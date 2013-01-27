a7c9deff4c0096d322bb4f1601eecc518424ae5a
AlBakerKY <baker.al@gmail.com>
2013-01-26 20:23:30 -0500

    df

---
+++ Assets/Mesh/Materials/Rotate.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(new Vector3(0,0,1), 1);
+	
+	}
+}
