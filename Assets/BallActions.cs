65f5bdeedff2d0c0289d281b85ce041174851561
AlBakerKY <baker.al@gmail.com>
2013-01-28 20:50:00 -0500

    Now check if ball has passed below a y value instead of intersecting a plane.  The plane remains in place as a footing for the pillars.

--- Assets/BallActions.cs
+++ Assets/BallActions.cs
@@ -6,6 +6,7 @@ public class BallActions : MonoBehaviour {
 	LevelBuilder levelBuilder;
 	public AudioClip flatline;
 	
+	public const float FLOOR_ELEVATION = -5f;  // This should ultimately be a nicer number, but simply replicates the existing plane.
 	// Use this for initialization
 	void Start () {		
 		levelBuilder = GameObject.Find("LevelBuilder").GetComponent<LevelBuilder>();			
@@ -13,15 +14,18 @@ public class BallActions : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (this.gameObject.transform.position.y < FLOOR_ELEVATION) {
+			Debug.Log("Restart Level");
+			audio.Play();
+			Application.LoadLevel(Application.loadedLevel);
+		}
 	
 	}
 	
 	void OnCollisionEnter(Collision other) {
 //		Debug.Log("collided with " + other.gameObject.name);
-		if (other.gameObject.name == "Plane") {
-			Debug.Log("Restart Level");
-			audio.Play();
-			Application.LoadLevel(Application.loadedLevel);
+		if (other.gameObject.name == "Plane") {		
+			// Currently abandoned.....delete after test.
 		} else if (other.gameObject.name == "Exit(Clone)") {
 			
 		}
